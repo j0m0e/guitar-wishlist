56430e2243e8bb3d616a91eab7bda2a351715ea1
j0m0e <jamieyerushalmi@gmail.com>
2017-04-05 22:43:28 -0400

    test

---
+++ src/main/java/com/guitar/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.guitar.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class TestController {
+	@RequestMapping("/test")
+	public String test() {
+		return "Test me once.";
+	}
+	
+}
+
+
+
+
+
---
+++ src/main/java/com/guitar/model/GuitarStub.java
@@ -0,0 +1,8 @@
+package com.guitar.model;
+
+public class GuitarStub {
+	String g = "guitar";
+	public String whatevs(){
+		return "whatevs";
+	}
+}
