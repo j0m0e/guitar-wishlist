5e956666fc795c334951ecc5186804f2f20f9d4b
j0m0e <jamieyerushalmi@gmail.com>
2017-04-28 11:50:59 -0400

    Added preliminary front end resources: views, vuejs, bootstrap.  Added
    Guitar Controller class.  Added spring dev tools--fixed caching for dev.
    Added application.properties file to resources.

--- src/main/java/com/guitar/controller/TestController.java
+++ src/main/java/com/guitar/controller/TestController.java
@@ -1,13 +1,14 @@
 package com.guitar.controller;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
-@RestController
+@Controller
 public class TestController {
-	@RequestMapping("/test")
+	@RequestMapping(value="/test", method=RequestMethod.GET)
 	public String test() {
-		return "Test me once.";
+		return "test";
 	}
 	
 }
--- src/main/java/com/guitar/data_stub/GuitarStub.java
+++ src/main/java/com/guitar/data_stub/GuitarStub.java
@@ -1,8 +1,47 @@
 package com.guitar.data_stub;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.guitar.model.Guitar;
 
 public class GuitarStub {
-private String g = "guitar";
-	public String whatevs(){
-		return "whatevs";
+	// static block makes few guitar objects
+	// a static hashmap that holds guitar objects
+	// method that outputs a list of those guitar objects getAllGuitars
+	// method that outputs a specific guitar object based on its id --getGuitar
+	// 
+	
+	private static Map<Long, Guitar> guitars = new HashMap<Long, Guitar>();
+	
+	static {
+	
+		Guitar a = new Guitar(1L, "Gibson", "Les Paul Studio", 2005, 
+				"http://www.musiciansfriend.com/guitars/gibson-2016-les-paul-studio-hp-electric-guitar", 
+				"this is the first guitar");
+		Guitar b = new Guitar(2L, "Fender", "Telecaster Thinline", 1976, 
+				"http://media.musiciansfriend.com/is/image/MMGS7/Vintage-Modified-72-Telecaster-Thinline-Maple-Neck-Electric-Guitar-Natural/J13801000002000-00-120x120.jpg", 
+				"this is the second guitar");
+		Guitar c = new Guitar(3L, "Gibson", "SG", 1961, 
+				"http://media.musiciansfriend.com/is/image/MMGS7/2017-SG-Standard-HP-Electric-Guitar-Ebony/J46351000002000-00-120x120.jpg", 
+				"this is the third guitar");
+		Guitar d = new Guitar(4L, "Fender", "Standard Telecaster", 1959, 
+				"http://www.musiciansfriend.com/guitars/fender-standard-telecaster-electric-guitar/h76527", 
+				"this is the fourth guitar");
+		guitars.put(1L, a);
+		guitars.put(2L, b);
+		guitars.put(3L, c);
+		guitars.put(4L, d);
 	}
+	
+	public static List<Guitar> getAllGuitars(){
+		return new ArrayList<Guitar>(guitars.values());
+	}
+	
+	public static Guitar get(Long id) {
+		return guitars.get(id);
+	}
+	
+	
 }
